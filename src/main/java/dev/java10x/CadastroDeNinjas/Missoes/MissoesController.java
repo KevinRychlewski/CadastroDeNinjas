6785033e7a1495600e34a96509adda3b32d22016
KevinSipioni <kevinsipioni13@gmail.com>
2025-07-07 00:58:05 -0300

    feat: Criar as rotas do controller com mock

--- src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesController.java
@@ -1,25 +1,43 @@
 package dev.java10x.CadastroDeNinjas.Missoes;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping
+@RequestMapping("missoes")
 public class MissoesController {
 
-    @GetMapping("/boasvindas/missoes")
+    @GetMapping("/boasvindas")
     public String boasVindas() {
         return "Essa é minha primeira mensagem nessa rota de Missões";
     }
 
+    // POST -- Mandar uma requisicao para criar uma missao
     // Adicionar missao (CREATE)
+    @PostMapping("/criar")
+    public String criarMissao() {
+        return "Missao criada com sucesso!";
+    }
 
+    // GET -- Mandar uma requisicao para mostrar as missoes
     // Mostrar todas as missoes (READ)
+    @GetMapping("/listar")
+    public String mostrarTodasAsMissoes() {
+        return "Lista de todas as missoes";
+    }
 
-    // Mostrar missao por ID (READ)
-
+    // PUT -- Mandar uma requisicao para alterar uma missao
     // Alterar dados das missoes (UPDATE)
+    @PutMapping("/alterar")
+    public String alterarMissaoPorId() {
+        return "Missao alterada com sucesso!";
+    }
+
+    // GET -- Mandar uma requisicao para mostrar uma missao por ID
+    // Deletar missao (DELETE)
+    @DeleteMapping("/deletar")
+    public String deletarMissao() {
+        return "Missao deletada com sucesso!";
+    }
 
 
 }
